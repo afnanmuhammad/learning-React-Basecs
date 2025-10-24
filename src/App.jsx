1f7add31962b011a1e4ed019a53e9f1382dda88c
Muhammad Afnan <muhammadafnan849@gmail.com>
2025-10-24 07:47:12 -0700

    use fragments

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,18 @@ function App() {
 
 
   return (
-    <div>
+    <>
       <ClassBaseComponent />
       <FunctionBaseComponent />
-    </div>
+      <p>🔍 Why Use Fragments?<br />
+        ✅ To avoid unnecessary (div) wrappers (called "div soup") <br />
+        ✅ To return multiple elements from a component<br />
+        ✅ Keeps HTML structure clean and lightweight<br />
+        🧠 In short:<br />
+        ✅Fragment is like an invisible wrapper in React — <br />
+        ✅it groups elements without adding anything extra to the HTML.
+      </p>
+    </>
   )
 }
 
